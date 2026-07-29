50b4a93e8bb8feb742edf559d9573f8802bdf719
benitex-dev <benitezjorgedev@gmail.com>
2026-07-29 16:46:30 -0300

    feat: configure Entity Framework Core with PostgreSQL

---
+++ src/DevPortfolioMVC.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DevPortfolioMVC.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevPortfolioMVC.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Project> Projects { get; set; } = null!;
+    }
+}
--- src/DevPortfolioMVC.Web/Program.cs
+++ src/DevPortfolioMVC.Web/Program.cs
@@ -1,4 +1,11 @@
+using DevPortfolioMVC.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
