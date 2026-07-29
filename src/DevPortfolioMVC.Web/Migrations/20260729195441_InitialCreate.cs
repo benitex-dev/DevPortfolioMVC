9d344bd48bb4f56875a690fd2ecc39ff98d49104
benitex-dev <benitezjorgedev@gmail.com>
2026-07-29 17:02:38 -0300

    feat: add initial database schema with Entity Framework

---
+++ src/DevPortfolioMVC.Web/Migrations/20260729195441_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DevPortfolioMVC.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Summary = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    Problem = table.Column<string>(type: "text", nullable: false),
+                    Development = table.Column<string>(type: "text", nullable: false),
+                    RepositoryUrl = table.Column<string>(type: "text", nullable: true),
+                    DemoUrl = table.Column<string>(type: "text", nullable: true),
+                    IsFeatured = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
